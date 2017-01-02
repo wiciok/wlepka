d1b8312f94834c890fa012efed641317c8cb0521
wiciok <wiciok1@gmail.com>
2017-01-02 21:20:28 +0100

    -ustawiona poprawnie wersja mobilna

--- loginpage.php
+++ loginpage.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- dla IE na WP -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>Wlepka - aplikacja do dzielenia się kodami źródłowymi i plikami tekstowymi</title>
     <link rel="stylesheet" type="text/css" href="main.css">
     <link rel="stylesheet" type="text/css" href="loginpage.css">
